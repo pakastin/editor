252a9af0f90782c91ceba43673d2353940bebe57
Juha Lindstedt <juha@pakastin.fi>
2018-12-11 23:32:51 +0200

    initial commit

---
+++ app/js/index.js
@@ -0,0 +1,82 @@
+const { ipcRenderer } = require('electron');
+
+const createElement = (tagName) => {
+  const element = document.createElement(tagName);
+  return element;
+};
+
+const $menu = createElement('menu');
+const $editor = createElement('textarea');
+
+$editor.onkeydown = e => {
+  if (e.key === 'Tab') {
+    e.preventDefault();
+
+    const { selectionStart, selectionEnd } = $editor;
+
+    $editor.value = $editor.value.slice(0, selectionStart) + '\t' + $editor.value.slice(selectionEnd);
+
+    $editor.selectionStart = $editor.selectionEnd = selectionStart + 1;
+  }
+};
+
+document.body.appendChild($menu);
+document.body.appendChild($editor);
+
+ipcRenderer.on('read', (e, data) => {
+  const { contents } = data;
+  $editor.textContent = contents;
+});
+
+ipcRenderer.send('files');
+ipcRenderer.on('files', (e, { dir, files }) => {
+  while ($menu.childNodes.length) {
+    $menu.removeChild($menu.firstChild);
+  }
+  renderFiles($menu, files);
+});
+
+function renderFiles (parent, files, depth = 0) {
+  for (const file of files) {
+    const $file = createElement('file');
+    const $filename = createElement('p');
+    const $children = createElement('children');
+
+    $filename.textContent = file.name;
+    $filename.ondblclick = () => {
+      if (file.isDir) {
+        ipcRenderer.send('files', { fullpath: file.fullpath, reset: true });
+        $menu.scrollTop = 0;
+      }
+    };
+    $filename.onclick = () => {
+      if (file.isDir) {
+        if (file.name === '..') {
+          ipcRenderer.send('files', { fullpath: file.fullpath, reset: true });
+        } else {
+          ipcRenderer.send('files', { fullpath: file.fullpath });
+        }
+      } else {
+        ipcRenderer.send('read', { fullpath: file.fullpath });
+      }
+    };
+
+    if (file.isDir) {
+      $filename.style.color = '#999';
+      $filename.style.fontWeight = '600';
+    } else {
+      $filename.style.color = '';
+      $filename.style.fontWeight = '';
+    }
+
+    $filename.style.marginLeft = `${depth * 0.5}rem`;
+
+    if (file.children) {
+      renderFiles($children, file.children, depth + 1);
+    }
+
+    $file.appendChild($filename);
+    $file.appendChild($children);
+    parent.appendChild($file);
+  }
+}
---
+++ app/main.js
@@ -0,0 +1,109 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const fs = require('fs');
+const { promisify } = require('util');
+const path = require('path');
+const readdir = promisify(fs.readdir);
+const readFile = promisify(fs.readFile);
+const stat = promisify(fs.stat);
+const state = {
+  files: [],
+  fullpathLookup: {}
+};
+
+function createWindow () {
+  const win = new BrowserWindow();
+
+  win.loadFile('index.html');
+}
+
+app.on('ready', createWindow);
+
+ipcMain.on('read', async (e, data) => {
+  const { fullpath } = data || {};
+
+  const stats = await stat(fullpath);
+
+  if (stats.size > 100000) {
+    e.sender.send('read', {
+      fullpath,
+      contents: 'FILE TOO BIG'
+    });
+    return;
+  }
+
+  const contents = await readFile(fullpath, { encoding: 'utf8' });
+
+  e.sender.send('read', {
+    fullpath,
+    contents
+  });
+});
+
+ipcMain.on('files', async (e, data) => {
+  const { fullpath, reset } = data || {};
+  const home = app.getPath('home');
+  const dir = fullpath || home;
+  const parsed = path.parse(dir);
+
+  if (reset || !data) {
+    if (parsed.root !== parsed.dir) {
+      state.files = [{
+        name: '..',
+        fullpath: path.resolve(dir, '..'),
+        isDir: true
+      }];
+    } else {
+      state.files = [];
+    }
+    state.fullpathLookup = {};
+  }
+
+  if (state.fullpathLookup[dir] && state.fullpathLookup[dir].children) {
+    // close dir
+    state.fullpathLookup[dir].children = null;
+
+    e.sender.send('files', {
+      dir,
+      files: state.files
+    });
+    return;
+  }
+
+  const files = await readdir(dir).then(files => {
+    return Promise.all(files.map(async file => {
+      const stats = await stat(path.join(dir, file));
+
+      return {
+        name: file,
+        fullpath: path.join(dir, file),
+        isDir: stats.isDirectory()
+      };
+    }));
+  });
+
+  files.forEach(file => {
+    state.fullpathLookup[file.fullpath] = file;
+  });
+
+  files.sort((a, b) => {
+    return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
+  });
+
+  if (state.fullpathLookup[dir]) {
+    // open dir
+    state.fullpathLookup[dir].children = files.filter(file => file.isDir).concat(files.filter(file => !file.isDir));
+
+    e.sender.send('files', {
+      dir,
+      files: state.files
+    });
+    return;
+  }
+
+  Array.prototype.push.apply(state.files, files.filter(file => file.isDir).concat(files.filter(file => !file.isDir)));
+
+  e.sender.send('files', {
+    dir,
+    files: state.files
+  });
+});
