c6a7c6a372ddf5c323cd501ee7ff98f3d9d565d2
Juha Lindstedt <juha@pakastin.fi>
2018-12-12 09:03:27 +0200

    Add queue

--- app/main.js
+++ app/main.js
@@ -18,92 +18,117 @@ function createWindow () {
 
 app.on('ready', createWindow);
 
-ipcMain.on('read', async (e, data) => {
-  const { fullpath } = data || {};
+const queue = [];
 
-  const stats = await stat(fullpath);
+const serve = () => {
+  queue.shift();
+  if (queue.length) {
+    queue[0]();
+  }
+}
 
-  if (stats.size > 100000000) {
+ipcMain.on('read', (e, data) => {
+  queue.push(async () => {
+    const { fullpath } = data || {};
+
+    const stats = await stat(fullpath);
+
+    if (stats.size > 100000000) {
+      e.sender.send('read', {
+        fullpath,
+        contents: 'FILE TOO BIG'
+      });
+      serve();
+      return;
+    }
+
+    const contents = await readFile(fullpath, { encoding: 'utf8' });
+
     e.sender.send('read', {
       fullpath,
-      contents: 'FILE TOO BIG'
+      contents
     });
-    return;
-  }
-
-  const contents = await readFile(fullpath, { encoding: 'utf8' });
-
-  e.sender.send('read', {
-    fullpath,
-    contents
+    serve();
   });
+  if (queue.length === 1) {
+    queue[0]();
+  }
 });
 
-ipcMain.on('files', async (e, data) => {
-  const { fullpath, reset } = data || {};
-  const home = app.getPath('home');
-  const dir = fullpath || home;
-  const parsed = path.parse(dir);
+ipcMain.on('files', (e, data) => {
+  queue.push(async () => {
+    const { fullpath, reset } = data || {};
+    const home = app.getPath('home');
+    const dir = fullpath || home;
+    const parsed = path.parse(dir);
 
-  if (reset || !data) {
-    if (parsed.root !== parsed.dir) {
-      state.files = [{
-        name: '..',
-        fullpath: path.resolve(dir, '..'),
-        isDir: true
-      }];
-    } else {
-      state.files = [];
+    if (reset || !data) {
+      if (parsed.root !== parsed.dir) {
+        state.files = [{
+          name: '..',
+          fullpath: path.resolve(dir, '..'),
+          isDir: true
+        }];
+      } else {
+        state.files = [];
+      }
+      state.fullpathLookup = {};
     }
-    state.fullpathLookup = {};
-  }
 
-  if (state.fullpathLookup[dir] && state.fullpathLookup[dir].children) {
-    // close dir
-    state.fullpathLookup[dir].children = null;
+    if (state.fullpathLookup[dir] && state.fullpathLookup[dir].children) {
+      // close dir
+      state.fullpathLookup[dir].children = null;
+
+      e.sender.send('files', {
+        dir,
+        files: state.files
+      });
+      serve();
+      return;
+    }
+
+    const files = await readdir(dir).then(files => {
+      return Promise.all(files.map(async file => {
+        const stats = await stat(path.join(dir, file));
+
+        return {
+          name: file,
+          fullpath: path.join(dir, file),
+          isDir: stats.isDirectory()
+        };
+      }));
+    });
+
+    files.forEach(file => {
+      state.fullpathLookup[file.fullpath] = file;
+    });
+
+    files.sort((a, b) => {
+      return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
+    });
+
+    if (state.fullpathLookup[dir]) {
+      // open dir
+      state.fullpathLookup[dir].children = files.filter(file => file.isDir).concat(files.filter(file => !file.isDir));
+
+      e.sender.send('files', {
+        dir,
+        files: state.files
+      });
+      serve();
+      return;
+    }
+
+    Array.prototype.push.apply(state.files, files.filter(file => file.isDir).concat(files.filter(file => !file.isDir)));
 
     e.sender.send('files', {
       dir,
       files: state.files
     });
-    return;
+
+    serve();
+  });
+  if (queue.length === 1) {
+    queue[0]();
   }
-
-  const files = await readdir(dir).then(files => {
-    return Promise.all(files.map(async file => {
-      const stats = await stat(path.join(dir, file));
-
-      return {
-        name: file,
-        fullpath: path.join(dir, file),
-        isDir: stats.isDirectory()
-      };
-    }));
-  });
-
-  files.forEach(file => {
-    state.fullpathLookup[file.fullpath] = file;
-  });
-
-  files.sort((a, b) => {
-    return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
-  });
-
-  if (state.fullpathLookup[dir]) {
-    // open dir
-    state.fullpathLookup[dir].children = files.filter(file => file.isDir).concat(files.filter(file => !file.isDir));
-
-    e.sender.send('files', {
-      dir,
-      files: state.files
-    });
-    return;
-  }
-
-  Array.prototype.push.apply(state.files, files.filter(file => file.isDir).concat(files.filter(file => !file.isDir)));
-
-  e.sender.send('files', {
-    dir,
-    files: state.files
-  });
 });
